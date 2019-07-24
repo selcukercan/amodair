80f29b35e1cec3885b8e53922e32dae5d0500cb4
selcukercan <ercans@ethz.com>
2019-07-24 23:52:16 +0200

    end-to-end through socket object

--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,51 @@
 #include <iostream>
 #include <stdlib.h>
 #include <cstring>
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <unistd.h>
+
 #include <string>
 #include "socket.h"
 
-
 using namespace std;
-// Socker Address, file-like.
-
-
-// Print Error
-#define handle_error(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0) 
 
 // Unix Socket Address Definition
-struct sockaddr_unix{
-sa_family_t sun_family;
-char        sun_path[108]; /* pathname */ }; 
+struct sockaddr_unix
+{
+	sa_family_t sun_family;
+	char sun_path[108]; /* pathname */
+};
 
+int main()
+{
+	string FD_ADDRESS = "/home/selcuk/amodair/sockets/socket_1";
+	const char *SOCKET_ADDRESS = FD_ADDRESS.c_str();
+	int BUFFER_SIZE = 12;
+	char buffer[BUFFER_SIZE];
+	int backlog = 1;
+	int SOCKET_TYPE = -1;
 
-int main() {
-	string tmp11 = "/home/selcuk/amodair/sockets/socket_1";
-	const char * SOCKET_ADDRESS = tmp11.c_str();
-	int BUFFER_SIZE = 12; 
-	// Close the connection if previous connection remained unclosed
-	unlink(SOCKET_ADDRESS);
-
-	Socket my_socket;
+	Socket my_socket(SOCKET_TYPE, FD_ADDRESS);
 	// Socket Description
 	int sfd = my_socket.initializeSocket(AF_UNIX, SOCK_STREAM, 0);
-	if(sfd == -1){
-		handle_error("Failed to create a file descriptor for the new socket");
-	}
-	else{
-		std::cout << "Successfully to create a socket!" << std::endl;
-	}
 
-	// Socket Address 
+	// Socket Address
 	sockaddr_unix socket;
 	socket.sun_family = AF_UNIX;
 	strncpy(socket.sun_path, SOCKET_ADDRESS, sizeof(socket.sun_path) - 1);
 
-
-	// Socket Binding 
+	// Attempt to bind the socket
 	int bind_stat = my_socket.bindSocket(sfd, socket);
-	// Check for binding success
-	if(bind_stat == -1){
-		 handle_error("Failed to bind");
-	}
-
+	
 	// Attempt to listen
-	// Allowed size of the queue for waiting for connecting **sfd** 
-	int backlog = 1;
 	int listening = my_socket.listenSocket(sfd, backlog);
 
-	if(listening == -1){
-		handle_error("Failed to listen");
-	}
-
 	// Attempt to accept
-	// Start accepting the calls on the socket
-	char buffer[BUFFER_SIZE]; 
-	sockaddr_unix  client_socket;
-	//int cfd = accept(sfd, (struct sockaddr *) &client_socket, (socklen_t*) &sizeof(client_socket)); 
-	int cfd = accept(sfd, NULL, NULL);
+	int cfd = my_socket.acceptCall(sfd, NULL, NULL);
 
-	while(true){
-	// Attemp to read
-	int reading = read(cfd, buffer, BUFFER_SIZE);
-	if (reading == -1) {
-		handle_error("Failed to read the buffer");
+	while (true)
+	{
+		// Attempt to read
+		int reading = my_socket.readSocket(cfd, buffer, BUFFER_SIZE);
+		printf(buffer);
 	}
-	// Ensure buffer is 0-terminated  
-	buffer[BUFFER_SIZE - 1] = 0; 
-	printf(buffer);
-	}
-
-	// Close the connection
-	unlink(SOCKET_ADDRESS); 
-}	
+}
--- src/socket.h
+++ src/socket.h
@@ -4,10 +4,14 @@
 #include <sys/socket.h>
 #include <stdexcept>
 #include <string>
-
+#include <unistd.h> // read()
 // Print Error
-#define handle_error(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0) 
-
+#define handle_error(msg)   \
+    do                      \
+    {                       \
+        perror(msg);        \
+        exit(EXIT_FAILURE); \
+    } while (0)
 
 using namespace std;
 using std::string;
@@ -15,7 +19,7 @@ using std::string;
 class Socket
 {
 public:
-    Socket();
+    Socket(int SOCKET_TYPE, string SOCKET_ADDRESS);
     ~Socket();
 
     int initializeSocket(int DOMAIN, int CONNECTION_TYPE, int protocol_type);
@@ -23,6 +27,7 @@ public:
     int bindSocket(int fd, T socket);
     int listenSocket(int sfd, int backlog);
     int acceptCall(int sfd, int caddr, int caddrlen);
+    int readSocket(int cfd, char *buffer, int BUFFER_SIZE);
 
 private:
     // socket initialization
@@ -31,7 +36,7 @@ private:
     int protocol_type_ = -1;
     // socket binding
     int fd = -1;
-    const char * SOCKET_ADDRESS_ = NULL;
+    const char *SOCKET_ADDRESS_ = NULL;
     // client interaction
     int backlog_ = -1;
     int sfd = -1;
@@ -39,74 +44,103 @@ private:
     int caddrlen = -1;
 };
 
-/* 
-struct socketConfig{
-    // socket initialization
-    int DOMAIN_ = NULL;
-    int CONNECTION_TYPE_ = NULL;
-    int protocol_type_ = NULL;
-    // socket binding
-    int fd = NULL;
-    string SOCKET_ADDRESS_ = NULL;
-    // client interaction
-    int backlog_ = NULL;
-    int sfd = NULL;
-    int caddr = NULL;
-    int caddrlen = NULL;
+Socket::Socket(int SOCKET_TYPE, string SOCKET_ADDRESS)
+{
+    SOCKET_ADDRESS_ = SOCKET_ADDRESS.c_str();
+    // Close the connection if previous connection remained unclosed
+    unlink(SOCKET_ADDRESS_);
 }
-*/
 
+Socket::~Socket()
+{
+    // Close the connection before the object is destroyed
+    unlink(SOCKET_ADDRESS_);
+}
 
-Socket::Socket() {}
+int Socket::initializeSocket(int DOMAIN, int CONNECTION_TYPE, int protocol_type)
+{
+    // Socket Initialization
+    // see: http://www.linuxhowtos.org/manpages/2/socket.htm
+    // socket(domain, type, protocol)
+    // The **domain** argument specifies a communication domain
+    //   AF_UNIX -> local communication
+    // The socket has the indicated **type**, which specifies the communication semantics.
+    //   SOCK_STREAM -> TCP
+    // The protocol indicates the **protocol** used for communication
+    //   0 -> Default Protocol
+    // socket(..) generates a files descriptor on linux
 
-Socket::~Socket() {}
+    int sfd = socket(DOMAIN, CONNECTION_TYPE, protocol_type);
 
-int Socket::initializeSocket(int DOMAIN, int CONNECTION_TYPE, int protocol_type){
-	// Socket Initialization
-	// see: http://www.linuxhowtos.org/manpages/2/socket.htm
-	// socket(domain, type, protocol)
-	// The **domain** argument specifies a communication domain
-	//   AF_UNIX -> local communication
-	// The socket has the indicated **type**, which specifies the communication semantics.
-	//   SOCK_STREAM -> TCP
-	// The protocol indicates the **protocol** used for communication
-	//   0 -> Default Protocol
-	// socket(..) generates a files descriptor on linux
+    // Check socket initialization success
+    if (sfd == -1)
+    {
+        handle_error("Failed to create a file descriptor for the new socket");
+    }
+    else
+    {
+        std::cout << "Successfully to create a socket!" << std::endl;
+    }
 
-	int sfd = socket(DOMAIN, CONNECTION_TYPE, protocol_type);
-
-	return sfd;
+    return sfd;
 }
 
 template <class T>
-int Socket::bindSocket(int fd, T socket){
+int Socket::bindSocket(int fd, T socket)
+{
     // Attempt to bind
-	// Bind generated socket **sfd** to socket address **socket** 
-	//int bind_stat = bind(sfd, (const struct sockaddr *) &socket, sizeof(struct sockaddr_unix));
-	//unlink(socket.sun_path); 
-    int bind_stat = bind(fd, (const struct sockaddr *) &socket, sizeof(T)); 
-	return bind_stat;
+    // Bind generated socket **sfd** to socket address **socket**
+    //int bind_stat = bind(sfd, (const struct sockaddr *) &socket, sizeof(struct sockaddr_unix));
+    //unlink(socket.sun_path);
+    int bind_stat = bind(fd, (const struct sockaddr *)&socket, sizeof(T));
+
+    // Check binding success
+    if (bind_stat == -1)
+    {
+        handle_error("Failed to bind");
+    }
+    return bind_stat;
 }
 
+int Socket::listenSocket(int sfd, int backlog)
+{
+    // Allowed size of the queue *backlog* for waiting for connecting **sfd**
+    int list_stat = listen(sfd, backlog);
 
-int Socket::listenSocket(int sfd, int backlog){
-	int list_stat = listen(sfd, backlog);
-	return list_stat;
-}
-	
-int Socket::acceptCall(int sfd, int caddr, int caddrlen){
-	// see: http://www.linuxhowtos.org/manpages/2/accept.htm
-	//The accept() system call is used with connection-based socket types (SOCK_STREAM, SOCK_SEQPACKET). 
-	//It extracts the first connection request on the queue of pending connections for the listening socket, 
-	//sockfd, creates a new connected socket, and returns a new file descriptor referring to that socket.
-	int scfd = 0; // TODO Is it a problem to initialize to 0?   
-
-	if(caddr == NULL){
-		int scfd = accept(sfd, NULL, NULL); // fd of the connected socket
-	}
-	else{
-		throw std::logic_error("Function not yet implemented"); 
-	}
-	return scfd;
+    // Check listening success
+    if (list_stat == -1)
+    {
+        handle_error("Failed to listen");
+    }
+    return list_stat;
 }
 
+int Socket::acceptCall(int sfd, int caddr, int caddrlen)
+{
+    // see: http://www.linuxhowtos.org/manpages/2/accept.htm
+    //The accept() system call is used with connection-based socket types (SOCK_STREAM, SOCK_SEQPACKET).
+    //It extracts the first connection request on the queue of pending connections for the listening socket,
+    //sockfd, creates a new connected socket, and returns a new file descriptor referring to that socket.
+    int scfd = 0; // TODO Is it a problem to initialize to 0?
+
+    if (caddr == NULL)
+    {
+        scfd = accept(sfd, NULL, NULL); // fd of the connected socket
+    }
+    else
+    {
+        throw std::logic_error("Function not yet implemented");
+    }
+    return scfd;
+}
+
+int Socket::readSocket(int cfd, char *buffer, int BUFFER_SIZE)
+{
+    int read_stat = read(cfd, buffer, BUFFER_SIZE);
+    if (read_stat == -1)
+    {
+        handle_error("Failed to read the buffer");
+    }
+
+    return read_stat;
+}
